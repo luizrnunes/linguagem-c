a3e2fca21540352e53210f2a3d4a93e6fdc18307
Luiz Henrique <luizhnrodrigues2007@gmail.com>
2025-09-02 20:33:13 -0300

    Fix:Correção da entrada de dados para a forma de pagamento

--- src/pagamento.c
+++ src/pagamento.c
@@ -4,9 +4,11 @@
 int main(){
     system("clear");
     float preco, resultado;
-    char forma_pg = '';
+    char forma_pg;
     printf("Digite o valor a ser pago e tecle enter\n");
     scanf("%f",&preco);
+
+    getchar();
 
     printf("Digita a forma de pagamento, sendo:\nc->crédito\nd->débito\nD->dinheiro\np->pix\n");
     scanf("%c",forma_pg);
@@ -15,18 +17,18 @@ int main(){
     if( forma_pg == 'c'){
         resultado = preco * (0.05 + 1);
         // resultado = preco * 0.05 + preco;
-        printf("O valor a ser pago no crédito é %f\n",resultado);
+        printf("O valor a ser pago no crédito é %.2f\n",resultado);
     }
     else if( forma_pg == 'd'){
-        printf("O valor a ser pago no débito é %f\n",preco);
+        printf("O valor a ser pago no débito é %.2f\n",preco);
     }
     else if( forma_pg == 'D'){
         resultado = preco * 0.98;
-        printf("O valor a ser pago no dinheiro é %f\n",resultado);
+        printf("O valor a ser pago no dinheiro é %.2f\n",resultado);
     }
     else if( forma_pg == 'p'){
         resultado = preco * 0.96;
-        printf("O valor a ser pago no pix é %f\n",resultado);
+        printf("O valor a ser pago no pix é %.2f\n",resultado);
     }
     else{
         printf("Esta forma de pagamento NÃO EXISTE!\n");
